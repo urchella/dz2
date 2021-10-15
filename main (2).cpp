cdb8e358317b8577662849d7f0116e1c096f4c37
urchella <91685952+urchella@users.noreply.github.com>
2021-10-15 21:57:39 +0300

    dz mystat

---
+++ main (2).cpp
@@ -0,0 +1,60 @@
+/*Шульга Юрий П43021*/
+#include <iostream>
+using namespace std;
+int main(){
+ setlocale(LC_ALL, "Russian");
+  /*zadacha 1
+int a;
+int b;
+cin >> a >> b;
+if(a % 2 == 0 && b % 2 == 1){cout << b;}
+else if ( a % 2 == 1 && b % 2 == 0) { cout << a;}
+*/
+
+/*
+int A;
+cin >> A;
+if(A > 0) { cout << " A Положительное";}
+else if (A < 0) { cout << " A Отрицательное ";}
+else if (A == 0) {cout << " A Равно нулю";}
+*/
+
+/*
+int A;
+cin >> A;
+if(A % 4 == 0) { cout << " Год високосный ";}
+else  { cout << " Год не високосный";}
+*/
+
+/*
+int A;
+int B;
+cin >> A >> B;
+double res;
+int ress;
+double resss;
+res = A / B;
+ress = ((A / 1.0) / (B / 1.0)) * 10 ;
+resss = ress % 10;
+if ((!(A == 0 && B == 0)) && (A % B) == 0) {
+ cout << "Первое число делится нацело на второе" << endl;
+ cout << res;
+}
+else if ((!(A == 0 && B == 0)) &&  (( A % B ) > 0 || (A % B ) < 0))  {
+
+cout << "Первое число  не делится нацело на второе" << endl;
+cout << res << " Остаток " << resss;
+
+}
+
+
+*/
+
+
+
+
+
+
+
+
+}
