56434a9af90039a7f83ce7417d80139a2d0a274e
urchella <91685952+urchella@users.noreply.github.com>
2021-11-02 20:43:09 +0300

    Add files via upload

---
+++ DZ while .cpp
@@ -0,0 +1,51 @@
+/*Шульга Юрий П43021*/
+#include <iostream>
+using namespace std;
+int main(){
+ setlocale(LC_ALL, "Russian");
+ /*    ZADACHA 1
+int a = 10;
+while (a <=20){
+cout << a * a << endl;
+ a++;
+}
+*/
+
+/*        ZADACHA 2
+ int a = 35;
+ int i;
+ int n;
+i = 0;
+n = 0;
+while (a < 87) { 
+  if(a % 7  == 1 ){cout << a << endl;}
+  else if (a % 7 == 2){cout << a << endl;}
+  else if (a % 7 == 5){cout << a << endl;}
+ a++;
+}*/
+
+/*        ZADACHA 3
+int n;
+int i;
+int b;
+cin >> n;
+i = 0;
+b = 0;
+while (i <n){
+i ++;
+b = b + i;
+
+}
+cout << b;
+*/
+
+/*         ZADACHA 4
+int a = 100;
+int n;
+int i;
+while (a < 1000){
+if(((a / 100) + ((a % 100) / 10) + ((a % 100) % 10)) ==15){cout << a << endl;}
+a++;
+}
+*/
+}
