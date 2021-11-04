88c3e2b48d0149872b5421db213e63cb65068b34
urchella <91685952+urchella@users.noreply.github.com>
2021-11-04 22:14:19 +0300

    Add files via upload

---
+++ massiv.cpp
@@ -0,0 +1,66 @@
+/*Шульга Юрий П43021*/
+#include <iostream>
+#include <ctime>
+using namespace std;
+int main(){
+ setlocale(LC_ALL, "Russian");
+  /*  ZADACHA 1 
+
+  srand(time(0));
+ const int n = 100;
+ int arr[n];
+ int a = 0;
+ for(int i = 0;i < n;i++)arr[i] = rand() % 100;
+ for(int i = 0;i < n;i++) {
+ a = a + arr[i];
+ }
+ cout << a << endl;
+ */
+
+
+
+
+/*  ZADACHA 2
+
+ srand(time(0));
+ const int n = 80;
+ int arr[n];
+ int a = 0;
+ for(int i = 0;i < n;i++)arr[i] = rand() % 100;
+ for(int i = 0;i < n;i++) {
+if(arr[i] > 0){
+  a = a + arr[i];
+}
+ }
+
+ cout << a << endl;
+ */
+/* ZADACHA 3
+
+ srand(time(0));
+ const int n = 60;
+ int arr[n];
+ int a = 0;
+ for(int i = 0;i < n;i++)arr[i] = rand() % 100;
+ for(int i = 0;i < n;i++) {
+ if(arr[i] % 3 == 0)
+   a++;
+ }
+ cout << a << ' ';
+
+ */
+ /*  ZADACHA 4
+
+srand(time(0));
+ const int n = 90;
+ int arr[n];
+ int a = 1;
+ for(int i = 0;i < n;i++)arr[i] = rand() % 100;
+ for(int i = 0;i < n;i++) {
+ if(arr[i] % 2 == 1)
+   a = a * arr[i];
+ }
+ cout << a << ' ';
+*/
+ }
+
