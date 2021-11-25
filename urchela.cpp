f35696847327c60bc1ce61805453d1d5dcadc60d
urchella <91685952+urchella@users.noreply.github.com>
2021-11-25 18:28:57 +0300

    Add files via upload

--- urchela.cpp
+++ urchela.cpp
@@ -1,394 +1,142 @@
-/*Шульга Юрий П43021*/
+﻿/*Шульга Юрий П43021*/
 #include <iostream>
+#include <ctime>
 using namespace std;
-int main() {
-	setlocale(LC_ALL, "Russian");
-
-/*	int a;
-	int b;
-	double S;
-	double R;
-	double P;
-	double C;
-	cout << "naiti vso";
-	cin >> a >> b ;
-	S = (a * a) + (b * b);
-	R = (a * a) - (b * b);
-	P = (a * a) * (b * b);
-	C = (a * a) / (b * b);
-	cout << S  << endl;
-	cout << R << endl;
-	cout << P << endl;
-	cout << C << endl;
-	*/
-
-
-
-
-
-
-
-	/*int a;
-	int b;
-	double S;
-	double R;
-	double P;
-	double C;
-	cout << "naiti vso";
-	cin >> a >> b;
-	S =abs(a + b) ;
-	R =abs(a - b) ;
-	P =abs(a * b) ;
-	C =abs(a / b) ;
-	cout << S << endl;
-	cout << R << endl;
-	cout << P << endl;
-	cout << C << endl;
-	*/
-
-	/*int a;
-	int b;
-	double c;
-	double P;
-	cout << "naiti vso";
-	cin >> a >> b;
-	c = sqrt((a * a) + (b * b));
-	P = a + b + c;
-	cout << c << endl;
-	cout << P << endl;
-
-	*/
-
-	/*int R1;
-	int R2;
-	double p;
-	double S1;
-	double S2;
-	double S3;
-	cout << "naiti vso";
-	cin >> R1 >> R2;
-	S1 = 3.14 * (R1 * R1);
-	S2 = 3.14 * (R2 * R2);
-	S3 = S1 - S2;
-	cout << S1 << endl;
-	cout << S2 << endl;
-	cout << S3 << endl;
-	*/
-
-	/*
-	int x1;
-	int x2;
-	int y1;
-	int y2;
-	double P;
-	cout << "naiti vso";
-	cin >> x1 >> x2 >> y1 >> y2;
-	P = sqrt(((x2 - x1) * (x2 + x1)) + ((y2 - y1) * (y2 + y1)));
-	cout << P << endl; 
-	*/
-
-	/*int x1;
-	int x2;
-	int y1;
-	int y2;
-	int x3;
-	int y3;
-	double P;
-	double p;
-	double a;
-	double b;
-	double c;
-	double S;
-	cout << "naiti vso";
-	cin >> x1 >> x2 >> y1 >> y2 >> x3 >> y3;
-	a = sqrt(((x2 - x1) * (x2 + x1)) + ((y2 - y1) * (y2 + y1)));
-	b = sqrt(((x3 - x2) * (x3 + x2)) + ((y3 - y2) * (y3 + y2)));
-	c = sqrt(((x3 - x1) * (x3 + x1)) + ((y3 - y1) * (y3 + y1)));
-	P = a + b + c;
-	p = P / 2;
-	S = sqrt(p * (p - a) * (p - b) * (p - c));
-	cout << P << endl;
-	cout << S << endl;
-	*/
-/*
-int x1;
-int x2;
-double P;
-cout << "naiti vso";
-cin >> x1 >> x2;
-P = abs(x2 - x1);
-cout << P << endl;
-*/
-
-/*int A;
-int B;
-double S;
-cout << "naiti vso";
-cin >> A >>B;
-S = A;
-A = B;
-B = S;
-
-cout << A << endl;
-cout << B << endl;
-*/
-
-
-/*int a;
-int b;
-int S;
-cout << "naiti vso";
-cin >> a >> b;
-S = sqrt(a * b);
-cout << S << endl;
-*/
-
-/*begin24
-int A;
-int B;
-int C;
-double S;
-cout << "naiti vso";
-cin >> A >> B >> C;
-S = A;
-A = B;
-B = C;
-C = S;
-S = A;
-
-
-cout << A << endl;
-cout << B << endl;
-cout << C << endl;
-*/
-
-
-/*int A;
-int B;
-int C;
-double S;
-cout << "naiti vso";
-cin >> A >> B >> C;
-C = S;
-
-
-cout << A << endl;
-cout << B << endl;
-cout << C << endl;
-*/
-
-
-/*int a;
-int b;
-double x1;
-double x2;
-double x3;
-cout << "naiti vso";
-cin >> a >> b ;
-x1 = -sqrt(-b / a);
-x3 = 0;
-x2 = sqrt(- b / a);
-
-cout << x1 << endl;
-cout << x2 << endl;
-cout << x3<< endl;
-*/
-
-
-
-/*begin 25
-int x;
-double y;
-cout << "naiti vso";
-cin >> x;
-y = 3 * (x * x * x * x * x * x) - 6 * (x * x) - 7;
-
-cout << y << endl;
-*/
-
-/*begin 26
-int x;
-double y;
-cout << "naiti vso";
-cin >> x;
-y = 4 * ((x - 3) * (x - 3) * (x - 3) * (x - 3) * (x - 3) * (x - 3)) - 7 * ((x - 3) * (x - 3) * (x - 3)) + 2;
-
-cout << y << endl;
-*/
-
-
-/*begin 27
-int A;
-double S2;
-double S4;
-double S8;
-cout << "naiti vso";
-cin >> A;
-S2 = A * A;
-S4 = S2 * S2;
-S8 = S4 * S4;
-
-cout << S8 << endl;
-*/
-
-/*begin28
-int A;
-double S2;
-double S3;
-double S5;
-double S10;
-double S15;
-cout << "naiti vso";
-cin >> A;
-S2 = A * A;
-S3 = S2 * A;
-S5 = S3 * S2;
-S10 = S5 * S5;
-S15 = S10 * S5;
-
-cout << S15 << endl;
-*/
-
-/*integer1
-int L;
-double x;
-cout << "naiti vso";
-cin >> L;
-x = L / 100;
-cout << L << endl;
-*/
-
-/*integer2
-int M;
-double x;
-cout << "naiti vso";
-cin >> M;
-x = M / 1000;
-cout << x << endl;
-*/
-
-/*integer3
-int B;
-double x;
-cout << "naiti vso";
-cin >> B;
-x = B / 1024;
-cout << x << endl;
-*/
-
-/*integer 4
-int A;
-int B;
-double x;
-cout << "naiti vso";
-cin >> A >> B;
-x = A / B;
-cout << x << endl;
-*/
-
-/*integer 5
-int A;
-int B;
-double x;
-cout << "naiti vso";
-cin >> A >> B;
-x = A % B;
-cout << x << endl;
-*/
-
-/*integer 6
-int A;
-double C;
-double O;
-cout << "naiti vso";
-cin >> A ;
-C = A / 10;
-O = A % 10;
-cout << C << endl;
-cout << O << endl;
-*/
-
-
-/*integer 7
-int A;
-double C;
-double O;
-double S;
-double P;
-cout << "naiti vso";
-cin >> A;
-C = A / 10;
-O = A % 10;
-S = C + O;
-P = C * O;
-cout << S << endl;
-cout << P << endl;
-*/
-
-/*integer 8 
-int A;
-int S;
-double C;
-double O;
-cout << "naiti vso";
-cin >> A;
-C = A / 10;
-O = A % 10;
-S = C;
-C = O;
-O = S;
-cout << C << endl;
-cout << O << endl;
-*/
-
-/*integer 9 
-int A;
-double x;
-cout << "naiti vso";
-cin >> A;
-x = A / 100;
-cout << x << endl;
-*/
-
-
-/*integer 10
-int A;
-int x3;
-int y1;
-double x1;
-double y4;
-cout << "naiti vso";
-cin >> A;
-y1 = A % 100;
-x1 = y1 % 10;
-y4 = A % 100;
-x3 = y4 / 10;
-cout << x1 << endl;
-cout << x3 << endl;
-*/
-
-	
-	
-	
-/*Шульга Юрий П43021*/
-/*Вариант 3*/
-	
-double a;
-double h;
-double S;
-cout << "Вычисление площади треугольника " << endl;
-cout << "Введите исходные данные :" << endl;
-cout << "Основание (см) -> ";
-cin >> a;
-cout << "Высота(см) -> ";
-cin >> h;
-S = (a * h) / 2;
-cout << "Площадь треугольника (см) -> ";
-cout << S ;
-cout << "кв.см. ";
-
-
+void fillRandom(int* fa, const int fn) {
+	for (int i = 0; i < fn; i++) {
+		fa[i] = rand() % 101 - 50;
+		cout << fa[i] << ' ';
+	}
+}
+void bubleSort(int* fa, const int fn) {
+	for (int i = 0; i < fn; i++) {
+		for (int j = 0; j < fn - 1; j++) {
+			if (fa[j] > fa[j + 1]) {
+				swap(fa[j], fa[j + 1]);
+			}
+		}
+	}
+}
+void print(int* fa, const int fn) {
+	for (int i = 0; i < fn; i++) {
+		cout << fa[i] << ' ';
+	}
+}
+void mas(int* fa, const int fn) {
+	for (int i = 0; i < fn; i++) {
+		fa[i] = fa[i];
+	}
 
 }
+int powerA3(int fa) {
+	int b = fa * fa * fa;
+	return b;
+}
+int main() {
+	setlocale(LC_ALL, "Russian");
+	int a;
+	cin >> a;
+	cout << powerA3(a) << endl;
+}
 
- 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+	const int n = 5;
+	const int m = 2;
+	const int k = 7;
+	int a[n];
+	int b[m];
+	int c[k];
+	int temp = 0;
+	srand(time(0));
+	fillRandom(a, n); 
+
+	cout << endl;
+
+	fillRandom(b, m);
+
+	cout << endl;
+
+	for (int i = 0; i < n; i++) {
+		c[i] = a[i];
+	}
+	for (int i = n; i < k; i++) {
+		c[i] = b[i - 5];
+	}
+	cout << endl;
+	bubleSort(c, k);
+	print(c, k);
+
+	*/
+
+
+
+
+
+	
+
+
+	
+
+
+
+
+
+
+	
+
+
+
+	
+	
+	
+	
+	
+
+
+
+  
+
+
+
+
+
+
